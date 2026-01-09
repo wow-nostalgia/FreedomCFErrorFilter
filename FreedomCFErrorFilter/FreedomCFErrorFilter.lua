58ef0e23fed01bf0ab88f54c09c93b3ab6009a4e
Malz <play.malz.123@gmail.com>
2026-01-09 07:49:46 +0200

    add cond to not replace C_Timer.After

--- FreedomCFErrorFilter/FreedomCFErrorFilter.lua
+++ FreedomCFErrorFilter/FreedomCFErrorFilter.lua
@@ -1,32 +1,33 @@
-local filterChatErrors = true
-
 if not C_Timer then
     C_Timer = {}
 end
 
-do
-    local timers = {}
-    local frame = CreateFrame("Frame")
+if not C_Timer.After then
+    do
+        local timers = {}
+        local frame = CreateFrame("Frame")
 
-    frame:SetScript("OnUpdate", function(self, elapsed)
-        for i = #timers, 1, -1 do
-            local t = timers[i]
-            t.delay = t.delay - elapsed
-            if t.delay <= 0 then
-                table.remove(timers, i)
-                pcall(t.func)
+        frame:SetScript("OnUpdate", function(self, elapsed)
+            for i = #timers, 1, -1 do
+                local t = timers[i]
+                t.delay = t.delay - elapsed
+                if t.delay <= 0 then
+                    table.remove(timers, i)
+                    pcall(t.func)
+                end
             end
-        end
-    end)
+        end)
 
-    function C_Timer.After(delay, func)
-        if type(delay) ~= "number" or type(func) ~= "function" then
-            return
+        function C_Timer.After(delay, func)
+            if type(delay) ~= "number" or type(func) ~= "function" then
+                return
+            end
+            table.insert(timers, { delay = delay, func = func })
         end
-        table.insert(timers, { delay = delay, func = func })
     end
 end
 
+local filterChatErrors = true
 
 local orig_SendChatMessage = SendChatMessage
 
